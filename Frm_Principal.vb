b86950bcabf4ccdb5aef068c6e854af453fa5bd8
Carlos <carlos.prontil.sjc2013@gmail.com>
2025-12-09 16:08:09 -0300

    TESTET

--- Frm_Principal.vb
+++ Frm_Principal.vb
@@ -2,5 +2,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Console.WriteLine("TESTE")
         Console.WriteLine("HELLO")
+        Console.WriteLine("HELLO")
+        Console.WriteLine("HELLO")
+        Console.WriteLine("HELLO")
     End Sub
 End Class
